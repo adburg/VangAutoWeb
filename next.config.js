a389b567d4686f5bbb5cd625426a90923bb0fd36
ab596213 <adrian.berget@hotmail.com>
2023-07-25 12:13:24 +0200

    config env removed

--- next.config.js
+++ next.config.js
@@ -4,10 +4,6 @@ const nextConfig = {
 };
 
 module.exports = {
-  env: {
-    GOOGLE_ADS: process.env.GOOGLE_ADS,
-    GOOGLE_ANALYTICS: process.env.GOOGLE_ANALYTICS
-  },
   webpack: (config, { isServer }) => {
     if (!isServer) {
       // don't resolve 'fs' module on the client to prevent this error on build --> Error: Can't resolve 'fs'
